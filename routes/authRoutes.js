8c7908434f01d8db4ba743b666f4420523dacd84
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-17 22:16:52 +0530

    feat: implement /me endpoint to return current user using auth middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { register, login, logout } = require("../controllers/authController");
 
+const protect = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 // ================== Public Routes ==================
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// ================== Protected Routes ==================
+router.get("/me", protect, (req, res) => res.status(200).json(req.user));
 
 module.exports = router;
